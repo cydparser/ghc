d80aa311ec30e285f37deeb2f7558d217bffd4d4
cydparser <cydparser@gmail.com>
2025-08-13 14:33:06 -0700

    Use pinned nixpkgs from rust/flake.lock

--- shell.nix
+++ shell.nix
@@ -1,7 +1,19 @@
 let
   sources = import ./npins;
 
-  pkgs = import sources.nixpkgs { };
+  nixpkgs =
+    let
+      flake-lock = builtins.fromJSON (builtins.readFile rust/flake.lock);
+
+      nixpkgs-locked = flake-lock.nodes.nixpkgs.locked;
+    in
+    builtins.fetchGit {
+      url = "https://github.com/${nixpkgs-locked.owner}/${nixpkgs-locked.repo}";
+      rev = nixpkgs-locked.rev;
+      shallow = true;
+    };
+
+  pkgs = import nixpkgs { };
 
   inherit (pkgs) lib;
 
